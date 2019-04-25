eae7bb44743c5c29528ecdef4ba53e0962fc56ca
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-04-25 20:53:48 +0700

    Add StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("EasyList"),
-        ),
-        body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/android.jpg'),
-          Text("Food Paradise")
-        ],),),
-      ),
+          appBar: AppBar(
+            title: Text("EasyList"),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('AnhVietPham'),
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('assets/android.jpg'),
+                    Text("Food Paradise")
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
