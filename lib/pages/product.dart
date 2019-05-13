bbd52bac008fb024d5b023a2711854c0d4eb5535
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2019-05-13 18:26:14 +0700

    Learning Flutter : Navigation lecture 70

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
+
+  final String title;
+  final String imageURL;
+
+  ProductPage(this.title, this.imageURL)
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Product Detail"),
+        title: Text(title),
       ),
       body: Column(
         children: <Widget>[
-          Text('Details!'),
-          RaisedButton(
-            child: Text('BACK'),
-            onPressed: () => Navigator.pop(context),
-          )
+          Image.asset(imageURL),
+          Container(padding: EdgeInsets.all(10.0), child: Text('Details!')),
+          Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: Theme
+                    .of(context)
+                    .accentColor,
+                child: Text(title),
+                onPressed: () => Navigator.pop(context),
+              ))
         ],
       ),
     );
