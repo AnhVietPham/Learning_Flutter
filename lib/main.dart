07326c04594fb21d64627873286267a22437c323
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2019-05-13 13:52:03 +0700

    Learning Flutter : Navigation 68 lesson

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
-import './product_manager.dart';
-import 'assignment_1/assignment.dart';
+import './pages/home.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange
-      ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("EasyList"),
-          ),
-          body: ProductManager()),
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: HomePage(),
     );
   }
 }
