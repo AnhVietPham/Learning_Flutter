85571fb0278b73cfddcf80dadfa9017de3b50f0d
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2019-05-14 10:40:54 +0700

    Learning Flutter : Navigation lecture 72

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
@@ -8,22 +9,29 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Column(
-        children: <Widget>[
-          Image.asset(imageURL),
-          Container(padding: EdgeInsets.all(10.0), child: Text('Details!')),
-          Container(
-              padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text(title),
-                onPressed: () => Navigator.pop(context, true),
-              ))
-        ],
+    return WillPopScope(
+      onWillPop: () {
+        print('Back button presses!');
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Column(
+          children: <Widget>[
+            Image.asset(imageURL),
+            Container(padding: EdgeInsets.all(10.0), child: Text('Details!')),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('DELETE'),
+                  onPressed: () => Navigator.pop(context, true),
+                ))
+          ],
+        ),
       ),
     );
   }
