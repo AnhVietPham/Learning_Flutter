e3fb61635c6933d6a5ddbe02b1762104b52f1209
AnhVietPham <pham.anh.viet.vlth@gmail.com>
2019-05-14 18:30:09 +0700

    Learning Flutter : Navigation lecture 74

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './pages/home.dart';
+import './pages/auth.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.deepOrange),
-      home: HomePage(),
+      home: AuthPage(),
     );
   }
 }
